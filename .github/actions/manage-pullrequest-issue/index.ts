7f6224b70d5225f908691d199ff696821b39cbd7
Erik Plachta <ErikPlachta@users.noreply.github.com>
2024-08-01 19:53:33 -0400

    add node types

--- .github/actions/manage-pullrequest-issue/index.ts
+++ .github/actions/manage-pullrequest-issue/index.ts
@@ -51,7 +51,11 @@ async function run(): Promise<void> {
 
     core.info(`Action completed successfully.`);
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    } else {
+      core.setFailed(String(error));
+    }
   }
 }
 
